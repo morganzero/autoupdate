eb319fd26a8266cd1349845069a41b0c05ce8d77
Morgan Zero <57449785+morganzero@users.noreply.github.com>
2021-02-06 11:57:47 +0100

    Create autoupdate.sh

---
+++ #!/bin/autoupdate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get update -yqq
+apt-get dist-upgrade -yqq
+apt-get upgrade -yqq
+apt-get autoclean -yqq
+apt-get autoremove -yqq
+apt update -yqq
+apt dist-upgrade -yqq
+apt upgrade -yqq
+apt autoclean -yqq
+apt autoremove -y
+echo done!
